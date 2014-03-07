345544b6ad2d6bca32c638779ab1ecd0b8a10a23
Nicolas Penin <nicolas.penin@2ioconseil.fr>
2014-03-07 09:56:15 +0100

    upgraded dsl packages not to build them in cloudbuild

--- Evaluant.Uss.SqlMapper.Designer/DslPackage/GeneratedCode/Package.cs
+++ Evaluant.Uss.SqlMapper.Designer/DslPackage/GeneratedCode/Package.cs
@@ -23,7 +23,7 @@ namespace Evaluant.Uss.SqlMapper.Mapping
 	/// <summary>
 	/// This class implements the VS package that integrates this DSL into Visual Studio.
 	/// </summary>
-	[VSShell::DefaultRegistryRoot("Software\\Microsoft\\VisualStudio\\11.0")]
+	[VSShell::DefaultRegistryRoot("Software\\Microsoft\\VisualStudio\\12.0")]
 	[VSShell::PackageRegistration(RegisterUsing = VSShell::RegistrationMethod.Assembly, UseManagedResourcesOnly = true)]
 	[VSShell::ProvideToolWindow(typeof(SqlMapperMappingExplorerToolWindow), MultiInstances = false, Style = VSShell::VsDockStyle.Tabbed, Orientation = VSShell::ToolWindowOrientation.Right, Window = "{3AE79031-E1BC-11D0-8F78-00A0C9110057}")]
 	[VSShell::ProvideToolWindowVisibility(typeof(SqlMapperMappingExplorerToolWindow), Constants.SqlMapperMappingEditorFactoryId)]
@@ -34,14 +34,16 @@ namespace Evaluant.Uss.SqlMapper.Mapping
 					"CF_TOOLBOXITEMCONTAINER,CF_TOOLBOXITEMCONTAINER_HASH,CF_TOOLBOXITEMCONTAINER_CONTENTS", 
 					"CreateExampleClassF1Keyword", 
 					"@ExampleElementToolboxBitmap;Evaluant.Uss.SqlMapper.Mapping.Dsl.dll", 
-					0xff00ff)]
+					0xff00ff,
+					Index = 0)]
 	[VSShell::ProvideStaticToolboxItem("Evaluant.Uss.SqlMapper.Mapping.SqlMapperMappingToolboxTab",
 					"@ExampleRelationshipToolboxItem;Evaluant.Uss.SqlMapper.Mapping.Dsl.dll", 
 					"Evaluant.Uss.SqlMapper.Mapping.ExampleRelationshipToolboxItem", 
 					"CF_TOOLBOXITEMCONTAINER,CF_TOOLBOXITEMCONTAINER_HASH,CF_TOOLBOXITEMCONTAINER_CONTENTS", 
 					"ConnectExampleRelationF1Keyword", 
 					"@ExampleRelationshipToolboxBitmap;Evaluant.Uss.SqlMapper.Mapping.Dsl.dll", 
-					0xff00ff)]
+					0xff00ff,
+					Index = 1)]
 	[VSShell::ProvideEditorFactory(typeof(SqlMapperMappingEditorFactory), 103, TrustLevel = VSShellInterop::__VSEDITORTRUSTLEVEL.ETL_AlwaysTrusted)]
 	[VSShell::ProvideEditorExtension(typeof(SqlMapperMappingEditorFactory), "." + Constants.DesignerFileExtension, 50)]
 	[VSShell::ProvideEditorLogicalView(typeof(SqlMapperMappingEditorFactory), "{7651A702-06E5-11D1-8EBD-00A0C90F26EA}")] // Designer logical view GUID i.e. VSConstants.LOGVIEWID_Designer
